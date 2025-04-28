f1cf55ac7fedcb6bdc4e4d4a6d1db37b8dbc7e73
b4cksl4sh <andrei.sh@ton.org>
2025-04-28 19:47:09 +0300

    done

--- sources/tests/proofs.spec.ts
+++ sources/tests/proofs.spec.ts
@@ -1,21 +1,34 @@
-import {Cell, toNano} from "@ton/core"
+import {toNano, beginCell} from "@ton/core"
 import {Blockchain} from "@ton/sandbox"
-import {findTransactionRequired, flattenTransaction} from "@ton/test-utils"
+import {findTransactionRequired, flattenTransaction, randomAddress} from "@ton/test-utils"
 import {createJettonVault} from "../utils/environment"
 // eslint-disable-next-line
 import {SendDumpToDevWallet} from "@tondevwallet/traces"
 import {createJettonVaultMessage} from "../utils/testUtils"
-import {JettonVault} from "../output/DEX_JettonVault"
+import {JettonVault, storeLPDepositPart} from "../output/DEX_JettonVault"
 import {LPDepositPartOpcode} from "../output/DEX_LiquidityDepositContract"
 describe("Proofs", () => {
     test("TEP89 proof should correctly work for discoverable jettons", async () => {
         const blockchain = await Blockchain.create()
-        //blockchain.verbosity.vmLogs = "vm_logs_verbose"
         // Our Jettons, used when creating the vault, that supports TEP-89
         const vaultSetup = await createJettonVault(blockchain)
 
         const _ = await vaultSetup.deploy()
-        const mockPayload = new Cell()
+        const mockPayload = beginCell()
+            .store(
+                storeLPDepositPart({
+                    $$type: "LPDepositPart",
+                    liquidityDepositContract: randomAddress(0), // Mock LP contract address
+                    additionalParams: {
+                        $$type: "AdditionalParams",
+                        minAmountToDeposit: 0n,
+                        lpTimeout: 0n,
+                        payloadOnSuccess: null,
+                        payloadOnFailure: null,
+                    },
+                }),
+            )
+            .endCell()
 
         const sendNotifyWithTep89Proof = await vaultSetup.jetton.transfer(
             vaultSetup.vault.address,
@@ -30,9 +43,6 @@ describe("Proofs", () => {
                 },
             ),
         )
-        await SendDumpToDevWallet({
-            transactions: sendNotifyWithTep89Proof.transactions as any,
-        })
 
         expect(sendNotifyWithTep89Proof.transactions).toHaveTransaction({
             to: vaultSetup.jetton.minter.address,
@@ -47,10 +57,10 @@ describe("Proofs", () => {
         const prooferAddress = flattenTransaction(replyWithWallet).to
         expect(sendNotifyWithTep89Proof.transactions).toHaveTransaction({
             from: prooferAddress,
-            op: JettonVault.opcodes.JettonNotifyWithActionRequest,
+            op: JettonVault.opcodes.TEP89ProofResponse,
             // As there was a commit() after the proof was validated
             success: true,
-            // However, probably there is not-null exit code, as we attached incorrect payload
+            // However, probably there is not-null exit code, as we attached the incorrect payload
         })
         expect(await vaultSetup.vault.getInited()).toBe(true)
     })
