c4c3b242bed207957da6ba4e8a32c1c958f9c2c8
Kaladin13 <335095@niuitmo.ru>
2025-06-04 17:59:29 +0300

    add series of swaps tests

--- sources/tests/swaps-math.spec.ts
+++ sources/tests/swaps-math.spec.ts
@@ -1,11 +1,9 @@
 import {Blockchain} from "@ton/sandbox"
 import {createJettonAmmPool} from "../utils/environment"
-import {beginCell, toNano} from "@ton/core"
-import {AmmPool, loadPayoutFromPool, loadSendViaJettonTransfer} from "../output/DEX_AmmPool"
+import {toNano} from "@ton/core"
+import {AmmPool} from "../output/DEX_AmmPool"
 // eslint-disable-next-line
 import {SendDumpToDevWallet} from "@tondevwallet/traces"
-import {JettonVault} from "../output/DEX_JettonVault"
-import {findTransactionRequired, flattenTransaction} from "@ton/test-utils"
 import {calculateAmountOut, calculateSwapResult} from "../utils/liquidityMath"
 
 const expectEqualTvmToJs = (expected: bigint, got: bigint) => {
@@ -97,5 +95,68 @@ describe("Swaps math", () => {
         // check reserves change
         expectEqualTvmToJs(aReserveAfter, res.reserveA)
         expectEqualTvmToJs(bReserveAfter, res.reserveB)
+    })
+
+    test("should correctly change reserves after series of swaps", async () => {
+        const blockchain = await Blockchain.create()
+
+        const {ammPool, vaultA, vaultB, swap, isSwapped, initWithLiquidity} =
+            await createJettonAmmPool(blockchain)
+
+        const initialRatio = BigInt(random(1, 100))
+
+        const amountARaw = toNano(random(1, 50))
+        const amountBRaw = amountARaw * initialRatio // 1 a == 2 b ratio
+
+        const amountA = isSwapped ? amountARaw : amountBRaw
+        const amountB = isSwapped ? amountBRaw : amountARaw
+
+        const depositor = vaultB.treasury.walletOwner
+
+        await initWithLiquidity(depositor, amountA, amountB)
+
+        // check that reserves correctly change after series of swaps
+        // this is different from the previous test since there could be
+        // an error with payouts that will appear only in the long term
+        for (let index = 0; index < 20; index++) {
+            const leftReserve = await ammPool.getLeftSide()
+            const rightReserve = await ammPool.getRightSide()
+
+            const reserveA = isSwapped ? rightReserve : leftReserve
+            const reserveB = isSwapped ? leftReserve : rightReserve
+
+            const amountToSwap = toNano(random(1, 50))
+            const expectedOutput = await ammPool.getExpectedOut(vaultA.vault.address, amountToSwap)
+
+            const res = calculateSwapResult(reserveA, reserveB, AmmPool.PoolFee, amountToSwap, 0n)
+
+            const swapResult = await swap(
+                amountToSwap,
+                "vaultA",
+                expectedOutput,
+                0n,
+                false,
+                null,
+                null,
+            )
+
+            // check that swap was successful
+            expect(swapResult.transactions).toHaveTransaction({
+                from: vaultA.vault.address,
+                to: ammPool.address,
+                op: AmmPool.opcodes.SwapIn,
+                success: true,
+            })
+
+            const leftReserveAfter = await ammPool.getLeftSide()
+            const rightReserveAfter = await ammPool.getRightSide()
+
+            const aReserveAfter = isSwapped ? rightReserveAfter : leftReserveAfter
+            const bReserveAfter = isSwapped ? leftReserveAfter : rightReserveAfter
+
+            // check reserves change
+            expectEqualTvmToJs(aReserveAfter, res.reserveA)
+            expectEqualTvmToJs(bReserveAfter, res.reserveB)
+        }
     })
 })
