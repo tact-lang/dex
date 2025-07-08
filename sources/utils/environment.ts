3f9622ab225f4f10cccbdb7c5b1cfefef6d103d7
B4ckSl4sh <91282981+Shvandre@users.noreply.github.com>
2025-07-08 16:43:14 +0300

    refactor(ton-vault): Removed admin address from state init, fixed security issue when forwarding user's payload (#98)

--- sources/utils/environment.ts
+++ sources/utils/environment.ts
@@ -267,9 +267,7 @@ export const createJettonVault: Create<VaultInterface<JettonTreasury>> = async (
 export const createTonVault: Create<VaultInterface<TonTreasury>> = async (
     blockchain: Blockchain,
 ) => {
-    const vaultOwner = await blockchain.treasury("vault-owner")
-
-    const vault = blockchain.openContract(await TonVault.fromInit(vaultOwner.address))
+    const vault = blockchain.openContract(await TonVault.fromInit())
 
     const wallet = await blockchain.treasury("wallet-owner")
 
